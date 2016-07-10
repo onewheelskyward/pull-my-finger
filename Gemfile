0f1ad1863ef74a76d5b0a92add639e169db2a1e4
Andrew Kreps <andrew.kreps@gmail.com>
2016-07-10 15:20:18 -0700

    Ruby++

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'rest-client'
+
+group :test do
+  gem 'coveralls'
+  gem 'simplecov'
+  gem 'rspec'
+  gem 'rack-test'
+end
---
+++ app.rb
@@ -0,0 +1,51 @@
+require 'sinatra/base'
+require 'sinatra/config_file'
+require 'rest-client'
+require 'base64'
+
+class App < Sinatra::Base
+  register Sinatra::ConfigFile
+  config_file 'config.yml'
+
+  before do
+    content_type 'application/json'
+  end
+
+  def check_auth(params)
+    unless settings.tokens.include? params[:token] and settings.team_domains.include? params[:team_domain]
+      puts "Token #{params[:token]} not found in #{settings.tokens} or #{params[:team_domain]} doesn't match #{settings.team_domains}"
+      false
+    end
+    true
+  end
+
+  get '/pulls*' do
+    # halt 400, '{"message": "Auth failed."}' unless check_auth(params)
+    responses = []
+    puts params[:response_url]
+    auth = 'Basic ' + Base64.encode64(settings.username + ':' + settings.password)
+
+    settings.repos.each do |repo|
+      uri = 'https://api.github.com/repos/' + settings.repo_owner + '/' + repo + '/pulls';
+      puts 'Calling URI: ' + uri
+
+      # result = RestClient.get uri
+      response = RestClient::Request.new(
+          :method => :get,
+          :url => uri,
+          :user => settings.username,
+          :password => settings.password,
+      ).execute
+      result = JSON.parse(response)
+      # var str_format = pull.head.repo.name + ': ' + pull.number + ' - ' + pull.title + '\n' + pull.url;
+      result.each do |pull|
+        str_format = "#{pull['head']['repo']['name']}: #{pull['number']} - #{pull['title']}\n#{pull['url']}"
+        responses.push str_format
+      end
+    end
+
+    { response_type: 'in_channel',
+      text: responses.join("\n"),
+    }.to_json
+  end
+end
